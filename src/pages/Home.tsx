b360f5a05a4d331a0dc3a7eb0416fb87d24f4996
LuisDevs <laldana161@gmail.com>
2025-10-13 15:28:08 -0600

    before tailwind upgrade

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* Theme Toggle */}
+
+      {/* Background Effect */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+}
