68c0dd0774df01077e54d69cc2ef7b6dec486249
LuisDevs <laldana161@gmail.com>
2025-10-13 20:57:46 -0600

    feat: add HeroSection component with animated introduction and call-to-action

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import StarBackground from "../components/StarBackground";
 import ThemeToggle from "../components/ThemeToggle";
